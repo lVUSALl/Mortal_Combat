338b34c7ff5b287b4954082e7ce2b74afd871b91
lVUSALl <vusalgasimov@outlook.com>
2022-09-21 15:06:26 +0300

    Site's version 1 Succes

---
+++ js/script.js
@@ -0,0 +1,266 @@
+function play1() {
+    var audio = new Audio( 'mp3/goro-wins.mp3');
+    audio.play();
+    document.getElementsByClassName('first-side')[0].style.display = 'none';
+    document.getElementById('fighters').style.display = 'none'; 
+    var mainimage = document.createElement('img');
+    mainimage.src =  'img/goro-main.jpg'
+    document.body.appendChild(mainimage)
+    document.getElementsByTagName('body')[0].style.display = 'flex'
+    mainimage.style.height = '750px'
+    document.getElementsByTagName('body')[0].style.justifyContent = 'center'
+}
+
+function play2() {
+    var audio = new Audio( 'mp3/johnny-cage.mp3');
+    audio.play();
+    document.getElementsByClassName('first-side')[0].style.display = 'none';
+    document.getElementById('fighters').style.display = 'none';
+    var mainimage = document.createElement('img');
+    mainimage.src =  'img/johnny-cage-main.webp'
+    document.body.appendChild(mainimage)
+    document.getElementsByTagName('body')[0].style.display = 'flex'
+    document.getElementsByTagName('body')[0].style.justifyContent = 'center'
+    document.getElementsByTagName('body')[0].style.alignItems = 'center'
+    document.getElementsByTagName('body')[0].style.marginTop = '125px'
+}
+
+function play3() {
+    var audio = new Audio( 'mp3/kano.mp3');
+    audio.play();
+    document.getElementsByClassName('first-side')[0].style.display = 'none';
+    document.getElementById('fighters').style.display = 'none';
+    var mainimage = document.createElement('img');
+    mainimage.src =  'img/kano-main.jpg'
+    document.body.appendChild(mainimage)
+    document.getElementsByTagName('body')[0].style.display = 'flex'
+    mainimage.style.height = '750px'
+    document.getElementsByTagName('body')[0].style.justifyContent = 'center'
+}
+
+function play4() {
+    var audio = new Audio( 'mp3/liu-kang.mp3');
+    audio.play();
+    document.getElementsByClassName('first-side')[0].style.display = 'none';
+    document.getElementById('fighters').style.display = 'none';
+    var mainimage = document.createElement('img');
+    mainimage.src =  'img/liu-kang-main.jpg'
+    document.body.appendChild(mainimage)
+    document.getElementsByTagName('body')[0].style.display = 'flex'
+    mainimage.style.height = '800px'
+    document.getElementsByTagName('body')[0].style.justifyContent = 'center'
+}
+
+function play5() {
+    var audio = new Audio( 'mp3/raiden.mp3');
+    audio.play();
+    document.getElementsByClassName('first-side')[0].style.display = 'none';
+    document.getElementById('fighters').style.display = 'none'; 
+    var mainimage = document.createElement('img');
+    mainimage.src =  'img/raiden-main.jpg'
+    document.body.appendChild(mainimage)
+    document.getElementsByTagName('body')[0].style.display = 'flex'
+    mainimage.style.height = '750px'
+    document.getElementsByTagName('body')[0].style.justifyContent = 'center'
+}
+
+function play6() {
+    var audio = new Audio( 'mp3/reptile.mp3');
+    audio.play();
+    document.getElementsByClassName('first-side')[0].style.display = 'none';
+    document.getElementById('fighters').style.display = 'none'; 
+    var mainimage = document.createElement('img');
+    mainimage.src =  'img/reptile-main.jfif'
+    document.body.appendChild(mainimage)
+    document.getElementsByTagName('body')[0].style.display = 'flex'
+    mainimage.style.height = '350px'
+    document.getElementsByTagName('body')[0].style.justifyContent = 'center'
+    mainimage.style.marginTop = '162px'
+}
+
+function play7() {
+    var audio = new Audio( 'mp3/scorpion.mp3');
+    audio.play();
+    document.getElementsByClassName('first-side')[0].style.display = 'none';
+    document.getElementById('fighters').style.display = 'none'; 
+    var mainimage = document.createElement('img');
+    mainimage.src =  'img/scorpion-main.jpg'
+    document.body.appendChild(mainimage)
+    document.getElementsByTagName('body')[0].style.display = 'flex'
+    mainimage.style.height = '750px'
+    document.getElementsByTagName('body')[0].style.justifyContent = 'center'
+}
+
+function play8() {
+    var audio = new Audio( 'mp3/shang-tsung.mp3');
+    audio.play();
+    document.getElementsByClassName('first-side')[0].style.display = 'none';
+    document.getElementById('fighters').style.display = 'none'; 
+    var mainimage = document.createElement('img');
+    mainimage.src =  'img/shang-tsung-main.webp'
+    document.body.appendChild(mainimage)
+    document.getElementsByTagName('body')[0].style.display = 'flex'
+    mainimage.style.height = '750px'
+    document.getElementsByTagName('body')[0].style.justifyContent = 'center'
+    mainimage.style.marginTop = '12px'
+}
+
+function play9() {
+    var audio = new Audio( 'mp3/sonya-blade.mp3');
+    audio.play();
+    document.getElementsByClassName('first-side')[0].style.display = 'none';
+    document.getElementById('fighters').style.display = 'none'; 
+    var mainimage = document.createElement('img');
+    mainimage.src =  'img/sonya-blade-main.gif'
+    document.body.appendChild(mainimage)
+    document.getElementsByTagName('body')[0].style.display = 'flex'
+    mainimage.style.height = '750px'
+    document.getElementsByTagName('body')[0].style.justifyContent = 'center'
+}
+
+function play10() {
+    var audio = new Audio( 'mp3/subzero.mp3');
+    audio.play();
+    document.getElementsByClassName('first-side')[0].style.display = 'none';
+    document.getElementById('fighters').style.display = 'none'; 
+    var mainimage = document.createElement('img');
+    mainimage.src =  'img/sub-zero-main.jpg'
+    document.body.appendChild(mainimage)
+    document.getElementsByTagName('body')[0].style.display = 'flex'
+    mainimage.style.height = '750px'
+    document.getElementsByTagName('body')[0].style.justifyContent = 'center'
+}
+
+function play11() {
+    var audio = new Audio( 'mp3/baraka.mp3');
+    audio.play();
+    document.getElementsByClassName('first-side')[0].style.display = 'none';
+    document.getElementById('fighters').style.display = 'none'; 
+    var mainimage = document.createElement('img');
+    mainimage.src =  'img/baraka-main.jpg'
+    document.body.appendChild(mainimage)
+    document.getElementsByTagName('body')[0].style.display = 'flex'
+    mainimage.style.height = '750px'
+    document.getElementsByTagName('body')[0].style.justifyContent = 'center'
+}
+
+function play12() {
+    var audio = new Audio( 'mp3/jade.mp3');
+    audio.play();
+    document.getElementsByClassName('first-side')[0].style.display = 'none';
+    document.getElementById('fighters').style.display = 'none'; 
+    var mainimage = document.createElement('img');
+    mainimage.src =  'img/jade-main.webp'
+    document.body.appendChild(mainimage)
+    document.getElementsByTagName('body')[0].style.display = 'flex'
+    mainimage.style.height = '750px'
+    document.getElementsByTagName('body')[0].style.justifyContent = 'center'
+}
+
+function play13() {
+    var audio = new Audio( 'mp3/jax.mp3');
+    audio.play();
+    document.getElementsByClassName('first-side')[0].style.display = 'none';
+    document.getElementById('fighters').style.display = 'none'; 
+    var mainimage = document.createElement('img');
+    mainimage.src =  'img/jax-main.jpg'
+    document.body.appendChild(mainimage)
+    document.getElementsByTagName('body')[0].style.display = 'flex'
+    mainimage.style.height = '750px'
+    document.getElementsByTagName('body')[0].style.justifyContent = 'center'
+}
+
+function play14() {
+    var audio = new Audio( 'mp3/kintaro.mp3');
+    audio.play();
+    document.getElementsByClassName('first-side')[0].style.display = 'none';
+    document.getElementById('fighters').style.display = 'none'; 
+    var mainimage = document.createElement('img');
+    mainimage.src =  'img/Kintaro-main.jpg'
+    document.body.appendChild(mainimage)
+    document.getElementsByTagName('body')[0].style.display = 'flex'
+    mainimage.style.height = '750px'
+    document.getElementsByTagName('body')[0].style.justifyContent = 'center'
+}
+
+function play15() {
+    var audio = new Audio( 'mp3/kitana.mp3');
+    audio.play();
+    document.getElementsByClassName('first-side')[0].style.display = 'none';
+    document.getElementById('fighters').style.display = 'none'; 
+    var mainimage = document.createElement('img');
+    mainimage.src =  'img/kitana-main.jpg'
+    document.body.appendChild(mainimage)
+    document.getElementsByTagName('body')[0].style.display = 'flex'
+    mainimage.style.height = '750px'
+    document.getElementsByTagName('body')[0].style.justifyContent = 'center'
+}
+
+function play16() {
+    var audio = new Audio( 'mp3/kung-lao.mp3');
+    audio.play();
+    document.getElementsByClassName('first-side')[0].style.display = 'none';
+    document.getElementById('fighters').style.display = 'none'; 
+    var mainimage = document.createElement('img');
+    mainimage.src =  'img/kung-lao-main.jpg'
+    document.body.appendChild(mainimage)
+    document.getElementsByTagName('body')[0].style.display = 'flex'
+    mainimage.style.height = '750px'
+    document.getElementsByTagName('body')[0].style.justifyContent = 'center'
+}
+
+function play17() {
+    var audio = new Audio( 'mp3/mileena.mp3');
+    audio.play();
+    document.getElementsByClassName('first-side')[0].style.display = 'none';
+    document.getElementById('fighters').style.display = 'none'; 
+    var mainimage = document.createElement('img');
+    mainimage.src =  'img/mileena-main.jpg'
+    document.body.appendChild(mainimage)
+    document.getElementsByTagName('body')[0].style.display = 'flex'
+    mainimage.style.height = '750px'
+    document.getElementsByTagName('body')[0].style.justifyContent = 'center'
+}
+
+function play18() {
+    var audio = new Audio( 'mp3/noob-saibot.mp3');
+    audio.play();
+    document.getElementsByClassName('first-side')[0].style.display = 'none';
+    document.getElementById('fighters').style.display = 'none'; 
+    var mainimage = document.createElement('img');
+    mainimage.src =  'img/noob-main.jpg'
+    document.body.appendChild(mainimage)
+    document.getElementsByTagName('body')[0].style.display = 'flex'
+    mainimage.style.height = '750px'
+    document.getElementsByTagName('body')[0].style.justifyContent = 'center'
+}
+
+function play19() {
+    var audio = new Audio( 'mp3/shao-kahn.mp3');
+    audio.play();
+    document.getElementsByClassName('first-side')[0].style.display = 'none';
+    document.getElementById('fighters').style.display = 'none'; 
+    var mainimage = document.createElement('img');
+    mainimage.src =  'img/shao-kahn-main.jpg'
+    document.body.appendChild(mainimage)
+    document.getElementsByTagName('body')[0].style.display = 'flex'
+    mainimage.style.height = '750px'
+    document.getElementsByTagName('body')[0].style.justifyContent = 'center'
+}
+
+function play20() {
+    var audio = new Audio( 'mp3/smoke.mp3');
+    audio.play();
+    document.getElementsByClassName('first-side')[0].style.display = 'none';
+    document.getElementById('fighters').style.display = 'none'; 
+    var mainimage = document.createElement('img');
+    mainimage.src =  'img/smoke-main.jpg'
+    document.body.appendChild(mainimage)
+    document.getElementsByTagName('body')[0].style.display = 'flex'
+    mainimage.style.height = '750px'
+    document.getElementsByTagName('body')[0].style.justifyContent = 'center'
+}
+
+function refresh() {
+    location.reload()
+}
